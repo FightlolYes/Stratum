7aeebfa5bc3be9f890c11a2711aab301254ef788
Yash Patil <yashpatil.uwu@gmail.com>
2024-01-26 23:43:42 +0530

    code refactoring and added sidebar in dashboard

--- src/routes/dashboard/dashboard.js
+++ src/routes/dashboard/dashboard.js
@@ -3,7 +3,7 @@ import authenticateToken from '../../middleware/auth.js'
 
 const router = express.Router()
 
-router.get('/', authenticateToken, async (req, res) => {
+router.get('/', async (req, res) => {
     res.render('dashboard/dashboard')
 })
 
